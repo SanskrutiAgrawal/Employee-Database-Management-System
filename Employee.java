86b38095dfb3784b92fa811d8b0a6887cb43153c
SanskrutiAgrawal <138443438+SanskrutiAgrawal@users.noreply.github.com>
2023-07-10 21:58:39 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+import javax.swing.*;
+public class EmployeeData {
+	public static Connection ConnectDB() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection
+					("jdbc:sqlite:C:\\Users\\Sanskruti Agrawal\\eclipse-workspace\\EmployeeData\\employee.db");
+			JOptionPane.showMessageDialog(null, "Connection Made !");
+			return conn;
+		}
+		catch(Exception e){
+			JOptionPane.showMessageDialog(null, "Connection Error !");
+			return null;
+		}
+	}
+   
+}
